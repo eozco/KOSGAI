fb1f5c78271a4ded99d99211f348be12d37f057a
eozco <90521742+eozco@users.noreply.github.com>
2025-04-02 13:16:53 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   server: {
-    host: "::",
+    host: true,
     port: 8080,
   },
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: true,
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
+      input: path.resolve(__dirname, "index.html"),
     },
   },
+});
 });
